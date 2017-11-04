cdd010fc6e7c570ca1484ae9e35f3d013701f6da
QuantumWaver <sholl76@gmail.com>
2017-11-04 19:18:28 -0400

    Add hello message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Through nature's inflexible grace, I'm learning to live."
+  end
+
 end
